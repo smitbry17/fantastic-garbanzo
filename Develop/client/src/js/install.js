24e38200aa37c4102929341bc8ca5c2312254b03
Bryan Smith <smitbry17@gmail.com>
2023-01-23 23:47:43 -0800

    final tests ran and all functionality works getting ready for heroku deployment

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -24,7 +24,6 @@ butInstall.addEventListener("click", async () => {
   butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
   console.log("App has been installed");
   window.deferredPrompt = null;
